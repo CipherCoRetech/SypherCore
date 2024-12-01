ed8d4c401b1b33b194964fb8a5bf5e939fa96ae3
SypherCoRe <189488300+SypherCoRe@users.noreply.github.com>
2024-11-30 18:03:48 -0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import sys
 import os
-
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.append(os.path.join(os.path.dirname(__file__), 'src'))
+from blockchain import Blockchain
 import argparse
 from src.blockchain import Blockchain
 from wallet.app import Wallet
